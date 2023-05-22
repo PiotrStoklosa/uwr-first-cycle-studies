adf4bc190c239c14b5d1938791cb6a8ac727163f
PiotrStoklosa <PiotrJStoklosa@gmail.com>
2023-05-22 15:49:45 +0200

    Update ASK

---
+++ semestr II/architektury-systemow-komputerowych/pracownia2-atak-hakerski/target/farm.c
@@ -0,0 +1,223 @@
+/* This function marks the start of the farm */
+int start_farm()
+{
+    return 1;
+}
+
+unsigned getval_433()
+{
+    return 2421742313U;
+}
+
+unsigned getval_140()
+{
+    return 3251079496U;
+}
+
+unsigned getval_457()
+{
+    return 3347663050U;
+}
+
+void setval_228(unsigned *p)
+{
+    *p = 3284633928U;
+}
+
+unsigned addval_186(unsigned x)
+{
+    return x + 3626964596U;
+}
+
+unsigned getval_212()
+{
+    return 2421732331U;
+}
+
+void setval_350(unsigned *p)
+{
+    *p = 3284633672U;
+}
+
+unsigned addval_413(unsigned x)
+{
+    return x + 3637031522U;
+}
+
+/* This function marks the middle of the farm */
+int mid_farm()
+{
+    return 1;
+}
+
+/* Add two arguments */
+long add_xy(long x, long y)
+{
+    return x+y;
+}
+
+unsigned addval_341(unsigned x)
+{
+    return x + 3465139292U;
+}
+
+unsigned addval_443(unsigned x)
+{
+    return x + 2447411528U;
+}
+
+unsigned addval_382(unsigned x)
+{
+    return x + 3525366025U;
+}
+
+void setval_306(unsigned *p)
+{
+    *p = 2425409160U;
+}
+
+void setval_336(unsigned *p)
+{
+    *p = 3682913929U;
+}
+
+unsigned getval_458()
+{
+    return 2447411528U;
+}
+
+unsigned getval_103()
+{
+    return 3269495112U;
+}
+
+unsigned getval_305()
+{
+    return 3269495112U;
+}
+
+unsigned addval_447(unsigned x)
+{
+    return x + 2430634312U;
+}
+
+void setval_419(unsigned *p)
+{
+    *p = 3525890441U;
+}
+
+unsigned getval_488()
+{
+    return 3286272328U;
+}
+
+unsigned getval_452()
+{
+    return 2497743176U;
+}
+
+void setval_345(unsigned *p)
+{
+    *p = 3281047049U;
+}
+
+unsigned getval_125()
+{
+    return 3374367401U;
+}
+
+unsigned getval_333()
+{
+    return 3674784393U;
+}
+
+unsigned getval_455()
+{
+    return 2429651347U;
+}
+
+unsigned addval_271(unsigned x)
+{
+    return x + 3536114057U;
+}
+
+unsigned addval_204(unsigned x)
+{
+    return x + 2425671305U;
+}
+
+unsigned getval_112()
+{
+    return 3674263945U;
+}
+
+void setval_116(unsigned *p)
+{
+    *p = 3526413961U;
+}
+
+void setval_408(unsigned *p)
+{
+    *p = 3374370313U;
+}
+
+void setval_117(unsigned *p)
+{
+    *p = 3351415160U;
+}
+
+unsigned addval_385(unsigned x)
+{
+    return x + 3680554633U;
+}
+
+unsigned getval_179()
+{
+    return 3286273352U;
+}
+
+unsigned addval_415(unsigned x)
+{
+    return x + 3374371209U;
+}
+
+unsigned addval_343(unsigned x)
+{
+    return x + 3674784385U;
+}
+
+unsigned addval_468(unsigned x)
+{
+    return x + 3281047961U;
+}
+
+unsigned getval_493()
+{
+    return 3676360969U;
+}
+
+void setval_237(unsigned *p)
+{
+    *p = 2425406089U;
+}
+
+unsigned addval_165(unsigned x)
+{
+    return x + 3525886601U;
+}
+
+void setval_346(unsigned *p)
+{
+    *p = 3529556617U;
+}
+
+unsigned getval_334()
+{
+    return 3264081073U;
+}
+
+/* This function marks the end of the farm */
+int end_farm()
+{
+    return 1;
+}
